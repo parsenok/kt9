0b5aef4dffc187d485cde6a76d9e21319a48d60b
Максим Симан <maksimsiman832@gmail.com>
2023-11-03 21:59:13 +0300

    Сделана 9кт

---
+++ kt9/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kt9
+{
+    class Calculator<T> where T : new ()
+    {
+        public static T Add(T x, T y)
+        {
+            dynamic X = x;
+            dynamic Y = y;
+            return X + Y;
+        }
+        public T Zero()
+        {
+            return new T();
+        }
+    }
+}
